64cdecd2c6c7660df754a6398c01bf7de5a2d63f
Akingbola Odunayo <akingbolaodun@gmail.com>
2025-12-01 13:04:13 +0100

    new commit

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { handlePaystackWebhook, initializePayment, verifyPayment } from '../controllers/payment.controller.js';
+import { handlePaystackWebhook, verifyPayment } from '../controllers/payment.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
 
 const paystackRoutes = express.Router();
-
-paystackRoutes.post('/initialize', initializePayment);
 
 paystackRoutes.post('/webhook/paystack', handlePaystackWebhook);
 paystackRoutes.get('/verify/:reference', verifyPayment);
